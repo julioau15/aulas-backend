d12e5fc9143a5d23a113e184c798382b1b2821dd
julioau15 <julioaugustov@gmail.com>
2026-02-04 22:15:13 -0300

    adicionado validação de valores negativos e arrumado logica das condições

--- exercicio-02/app.js
+++ exercicio-02/app.js
@@ -48,33 +48,35 @@ entradaDeDados.question('Por favor, digite o nome do cliente: ', function(client
                         let tempoPagamento = tempo
                         let montanteFinal
 
-                        if(!isEmpty(nomeCliente, nomeProduto, valorCompra, taxaJuros, tipoTempo, tempoPagamento)){
-                            console.log('[ERRO] Os campos devem ser preenchidos.')
+                        if(isEmpty(nomeCliente, nomeProduto, valorCompra, taxaJuros, tipoTempo, tempoPagamento)){
+                            console.log('[ERRO] Os campos devem ser preenchidos!')
+                            entradaDeDados.close()
                         }else if(!isNumber(valorCompra, taxaJuros, tempoPagamento)){
-                            console.log('[ERRO] o Valor de compra, a taxa de juros e o tempo de pagamento devem ser números.')
+                            console.log('[ERRO] o Valor de compra, a taxa de juros e o tempo de pagamento devem ser números!')
+                            entradaDeDados.close()
+                        }else if(valorCompra < 0 || taxaJuros < 0 || tempoPagamento){
+                            console.log('[ERRO] Os valores ser positivos!')
+                            entradaDeDados.close()
                         }else{
-                            if(tipoTempo.toLowerCase == 'meses' || tipoTempo.toLowerCase == 'mes' || tipoTempo.toLowerCase == 'mês'){
-                                taxaJuros = taxa
+                            if(tipoTempo.toLowerCase() == 'meses' || tipoTempo.toLowerCase() == 'mes' || tipoTempo.toLowerCase() == 'mês'){
                                 montanteFinal = calcularMontanteFinal(valorCompra, taxaJuros, tempoPagamento)
-                                montarMensagem(nomeCliente, nomeProduto, valorCompra, taxaJuros, tipoTempo, tempoPagamento, montanteFinal)
+                                montarMensagem(nomeCliente, nomeProduto, valorCompra, tempoPagamento, montanteFinal)
+                                entradaDeDados.close()
 
-                            }else if(tipoTempo.toLowerCase == 'anos' || tipoTempo.toLowerCase == 'ano'){
-                                taxaJuros = Number(taxa)/12
-                                montanteFinal = calcularMontanteFinal(valorCompra, taxaJuros, tempoPagamento)
-                                montarMensagem(nomeCliente, nomeProduto, valorCompra, taxaJuros, tipoTempo, tempoPagamento, montanteFinal)
+                            }else if(tipoTempo.toLowerCase() == 'anos' || tipoTempo.toLowerCase() == 'ano'){
+                                let taxaJurosMes = Number(taxaJuros)/12
+                                let tempoPagamentoMes = (Number(tempoPagamento) * 12)
+                                montanteFinal = calcularMontanteFinal(valorCompra, taxaJurosMes, tempoPagamentoMes)
+                                montarMensagem(nomeCliente, nomeProduto, valorCompra, tempoPagamentoMes, montanteFinal)
+                                entradaDeDados.close()
                             }else{
-                                console.log('[ERRO] Informe um valor valido')
+                                console.log('[ERRO] o tempo de pagamento deve ser meses ou anos!')
+                                entradaDeDados.close()
                             }
                         }
-
-                        
-                        
-                    })
-                    
-                })
-                
-            })
-            
+                    })                 
+                })         
+            })         
         })
     })
 })
@@ -96,15 +98,18 @@ function isNumber(valorCompra, taxaJuros, tempoPagamento){
 }
 
 function calcularMontanteFinal(valorCompra, taxaJuros, tempoPagamento){
-    return valorCompra * (1 + taxaJuros) ** tempoPagamento 
+    let i = Number(taxaJuros) / 100
+    let c = Number(valorCompra)
+    let n = Number(tempoPagamento)
+    return c * (1 + i) ** n 
 }
 
-function montarMensagem(nomeCliente, nomeProduto, valorCompra, taxaJuros, tipoTempo, tempoPagamento, montanteFinal){
+function montarMensagem(nomeCliente, nomeProduto, valorCompra,tempoPagamento, montanteFinal){
     console.log(`******************* [Viva Moda] *******************`)
     console.log(`Muito obrigado por realizar a sua compra conosco Sr(a) ${nomeCliente}`)
-    console.log(`A compra do produto ${nomeProduto}, tem um valor de: ${valorCompra}`)
-    console.log(`A sua compra será parcelada em ${tempoPagamento} vezes e o Sr(a) pagará: ${montanteFinal}.`)
-    console.log(`O acréscimo realizado ao valor de: ${valorCompra} será de ${}.\n`)
-    console.log(`Muito obrigado por escolher a [Viva Moda].`)
+    console.log(`A compra do produto ${nomeProduto}, tem um valor de: R\$${Number(valorCompra).toFixed(2)}`)
+    console.log(`A sua compra será parcelada em ${tempoPagamento} vezes e o Sr(a) pagará: R\$${Number(montanteFinal).toFixed(2)}.`)
+    console.log(`O acréscimo realizado ao valor de: R\$${Number(valorCompra).toFixed(2)} será de R\$${(Number(montanteFinal) - Number(valorCompra)).toFixed(2)}.\n`)
+    console.log(`Muito obrigado por escolher a Viva Moda.`)
     console.log(`*******************************************************`)
 }
