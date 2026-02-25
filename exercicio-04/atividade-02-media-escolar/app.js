bce79bbb6b6096462eb02a365fa0574797aa9b4f
julioau15 <julioaugustov@gmail.com>
2026-02-25 17:08:41 -0300

    iniciado atividade 02 no execicio 04

--- exercicio-04/atividade-02-media-escolar/app.js
+++ exercicio-04/atividade-02-media-escolar/app.js
@@ -1,16 +1,28 @@
-/***********************************************************************
- * Objetivo: Arquivo responsavel por 
+/*****************************************************************************************
+ * Objetivo: Arquivo responsavel pelas entradas de dados e saida de relatório no projeto
  * Data: 25/02/2026
  * Autor: Julio
  * Versão: 1.0
- * ********************************************************************/
+ * ***************************************************************************************/
 
 /**
- * 
- * 
- * 
- * 
- * 
+ *  - Receber NomeAluno, NomeProfessor, SexoAluno, SexoProfessor, NomeCurso, NomeDiciplina, 4 notas
+ *  - As notas devem ser entre 0 e 100
+ *  - Tratar campos vazios
+ *  - Calcular média
+ *  - Status do aluno: 
+ *      acima de 70 = Aprovado
+ *      abaixo de 50 = Reprovado
+ *      entre 50 e 69 = Exame
+ *  - Solicitar Nota Exame
+ *  - Se a nota do exame for acima de 60, aluno aprovado no exame
+ *  - Relatório do Aluno:
+ *  `O Aluno ${nomeAluno}` foi ${status} na diciplina ${nomeDiciplina}.
+ *  `Curso: ${nomeCurso}`
+ *  `Professor: ${nomeProfessor}`
+ *  `Notas do aluno: ${nota1}, ${nota2}, ${nota3}, ${nota4}, ${notaExame}`
+ *  `Média Final: ${mediaFinal}`
+ *  `Média final do exame: ${mediaFinalExame}`
  */
 
 
@@ -23,6 +35,34 @@ let entradaDeDados = readline.createInterface({
 })
 
 // entrada de dados
-entradaDeDados.question('', function(){
-
+entradaDeDados.question('Por favor, digite o nome do aluno: ', function(nomeA){
+    let nomeAluno = nomeA
+    entradaDeDados.question('Por favor, digite o sexo do aluno: ', function(sexoA){
+        let sexoAluno = sexoA
+        entradaDeDados.question('Por favor, digite o nome do professor: ', function(nomeP){
+            let nomeProfessor = nomeP
+            entradaDeDados.question('Por favor, digite o sexo do professor: ', function(nomeP){
+                let nomeProfessor = nomeP
+                entradaDeDados.question('Por favor, digite o nome do curso: ', function(nomeC){
+                    let nomeCurso = nomeC
+                    entradaDeDados.question('Por favor, digite o nome da diciplina: ', function(nomeD){
+                        let nomeDiciplina = nomeD
+                        entradaDeDados.question('Por favor, digite a nota 1: ', function(n1){
+                            let nota1 = n1
+                            entradaDeDados.question('Por favor, digite a nota 2: ', function(n2){
+                                let nota2 = n2
+                                entradaDeDados.question('Por favor, digite a nota 3: ', function(n3){
+                                    let nota3 = n3
+                                    entradaDeDados.question('Por favor, digite a nota 4: ', function(n4){
+                                        let nota4 = n4
+                            
+                                    })
+                                })
+                            })
+                        })
+                    })
+                })
+            })
+        })
+    })
 })
