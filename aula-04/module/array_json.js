03fd66e034bb176ff5a10a6969803a775ea93031
julioau15 <julioaugustov@gmail.com>
2026-03-13 11:27:29 -0300

    atualizado aula 04 - arrays

--- aula-04/module/array_json.js
+++ aula-04/module/array_json.js
@@ -108,6 +108,12 @@ const manipularDados = () => {
 
     console.log('\nFornecedores:')
     console.table(listaDeFornecedores)
+
+    // Permite adicionar novo elemento em um determidado lugar do array
+        // sploce(indice, 0, -> siginifica que não sera removido ninguem
+        
+    listaDeFornecedores.splice(2,0,'Carlos da Silva')
+    console.table(listaDeFornecedores)
 }
 
 const removerElemento = (dado, array) => {
@@ -136,14 +142,7 @@ const removerElemento = (dado, array) => {
 // Verifica a existencia de um elemento em uma lista
 const isInList = (dado, array) =>  array.includes(String(dado).toLowerCase())
 
-// if (isInList(dado, array)){
-//     removerElemento(dado, array)
-//     console.log('item removido com sucesso')
-//     console.table(array)
-// }else
-//     console.log('item não encontrado')
-
-
+// Verifica a quantidade de um determinado conteudo em um array 
 const quantidadeItens = (dado, array) => {
     let elemento = String(dado).toLowerCase()
     let lista = array
@@ -156,7 +155,110 @@ const quantidadeItens = (dado, array) => {
 
     return cont
 }
-let dado = 'josé'
-let array = listaDeNomes
 
-// console.log(`Quantidade de itens: ` + quantidadeItens(dado,array))
+// Cria um objeto JSON
+const criandoDadosJSON = () => {
+    let aluno = {
+        "nome": "Julio da Silva",
+        "data_nascimento": "2007-10-15",
+        "ra": 123456,
+        "telefone": "973496065863",
+        "email": "julio@gmail.com"
+    }
+
+    // Exibindo o objeto JSON completo
+    console.log(aluno)
+
+    // Exibindo apenas um atributo do jSON
+    console.log(aluno.nome)
+
+    // Adiciona um novo atributo no JSON
+    aluno.sexo = "M"
+    console.log(aluno)
+
+    // Deleta um atributo no JSON
+    delete aluno.telefone
+    console.log(aluno)
+}
+
+const cadastroDeProdutos = () => {
+    let marcas = [
+        {"id": 1, "marca": "Lg",         "telefone": "9493694598", "email": "lg@gmail.com"},         // indice 0
+        {"id": 2, "marca": "Lenovo",     "telefone": "9493694598", "email": "lenovo@gmail.com"},     // indice 1
+        {"id": 3, "marca": "Intel",      "telefone": "9493694598", "email": "intel@gmail.com"},      // indice 2
+        {"id": 4, "marca": "Apple",      "telefone": "9493694598", "email": "apple@gmail.com"},      // indice 3
+        {"id": 5, "marca": "Samsung",    "telefone": "9493694598", "email": "samsung@gmail.com"},    // indice 4
+        {"id": 6, "marca": "Rayzer",     "telefone": "9493694598", "email": "rayzer@gmail.com"},     // indice 5
+        {"id": 7, "marca": "Logitec",    "telefone": "9493694598", "email": "logitec@gmail.com"},    // indice 6
+        {"id": 8, "marca": "Dell",       "telefone": "9493694598", "email": "dell@gmail.com"},       // indice 7
+        {"id": 9, "marca": "Multilazer", "telefone": "9493694598", "email": "multilazer@gmail.com"}, // indice 8
+    ]
+
+    let cores = [
+        {"id": 1, "cor": "Branco"},   // indice 0
+        {"id": 2, "cor": "Preto"},    // indice 1
+        {"id": 3, "cor": "Azul"},     // indice 2
+        {"id": 4, "cor": "Rosa"},     // indice 3
+        {"id": 5, "cor": "Cinza"},    // indice 4
+        {"id": 6, "cor": "Amarelo"},  // indice 5
+        {"id": 7, "cor": "Verde"},    // indice 6
+        {"id": 8, "cor": "Marrom"},   // indice 7
+        {"id": 9, "cor": "Vermelho"}, // indice 8
+    ]
+
+    let produtos = 
+    [
+        {
+            "id": 1,
+            "nome": "Monitor",
+            "descricao": "27 polegadas",
+            "marca": [marcas[7].marca],
+            "quantidade": 20,
+            "cor": [cores[4], cores[1]],
+            "valor": 800.50
+        },
+        {
+            "id": 2,
+            "nome": "Teclado",
+            "descricao": "Teclado Mecânico rgb",
+            "marca": [marcas[6].marca],
+            "quantidade": 15,
+            "cor": cores,
+            "valor": 174.98
+        },
+        {
+            "id": 3,
+            "nome": "Mouse",
+            "descricao": "Mouse com fio",
+            "marca": [marcas[0].marca, marcas[7].marca, marcas[6].marca],
+            "quantidade": 476,
+            "cor": [cores[4], cores[1], cores[0]],
+            "valor": 278.54
+        }
+    ]
+
+    // console.log(produtos[0].cor)
+    // console.log(produtos[0].cor[1].cor)
+
+    // produtos[0].cor.forEach(cor => console.log(`A cor do produto é: ${cor.cor}`))
+
+
+    produtos.forEach(produto => {
+        console.log("-------------------------")
+        console.log(`\nProduto: ${produto.nome}`)
+        console.log(`Descrição: ${produto.descricao}`)
+        console.log(`Quantidade: ${produto.quantidade}`)
+        console.log(`Valor: R\$${produto.valor}`)
+
+        console.log('\nCor(es):')
+        produto.cor.forEach(cor => console.log(cor.cor))
+        
+        console.log('\nMarca(s):')
+        produto.marca.forEach(marca => console.log(marca))
+    })
+    console.log('-------------------------')
+
+
+}
+
+cadastroDeProdutos()
