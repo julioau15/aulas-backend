db098b9c53fa7a98cecb22d9e85c8b2f51dc6571
julioau15 <julioaugustov@gmail.com>
2026-03-18 10:33:42 -0300

    Adicionado exercicio 05 com função (getListaDeEstados)

---
+++ exercicio-05/module/manipular_dados.js
@@ -0,0 +1,34 @@
+/***********************************************************************
+ * Objetivo: Arquivo responsável manipular dados do projeto 
+ * Data: 18/03/2026
+ * Autor: Julio
+ * Versão: 1.0
+ * ********************************************************************/
+
+/**
+    Requsitos:
+        - Função (getListaDeEstados) que ira retornar todas as siglas dos estados
+        - Função (getDadosEstado) que ira retornar as informações do estado recebendo como parametro a sigla do Estado
+        - Função (getCapitalEstado) que ira retornar as informações da captal recebendo como parametro a sigla do Estado
+        - Função (getEstadosRegiao) que ira retornar os estados de determinada região rebendo como parametro a região
+        - Função (getCapitalPais) que ira retornar as capitais do Brasil
+        - Função (getCidades) que ira retornar as cidades de determidado estado rebendo como parametro sigla do Estado
+ */
+
+const listaDeEstados = require('./estados_cidades')
+
+// retorna as siglas dos estados e a quantidade de estados
+const getListaDeEstados = () => {
+    let estados = {
+        "uf": [],
+        "quantidade":0
+    }
+
+    listaDeEstados.estados.forEach(estado => {
+        estados.uf.push(estado.sigla)
+        estados.quantidade++
+    })
+
+    return estados
+}
+
